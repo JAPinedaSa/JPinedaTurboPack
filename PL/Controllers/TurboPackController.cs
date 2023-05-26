7dac51d59442c5fa781579cca8139006f91948ee
JAPinedaSa <JA.PinesaSa@gmail.comJA.PinesaSa@gmail.com>
2023-05-26 11:03:38 -0600

    Add project files.

---
+++ PL/Controllers/TurboPackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class TurboPackController : Controller
+    {
+        // GET: TurboPack
+        public ActionResult GetUsers()
+        {
+            return View();
+        }
+    }
+}
